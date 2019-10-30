2050d4f9e682744505c3dcab9359a8534f3e574c
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-10-30 17:29:52 -0300

    wip

--- scripts/paste-client-key-in-auditing-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-KEY_PATH=$1
-
-CONTAINER_NAME="auditing-server"
-
-sudo docker exec $CONTAINER_NAME /bin/bash -c "mkdir /root/auditing-server/src/main/resources/private/clientkeys"
-sudo docker cp $KEY_PATH $CONTAINER_NAME:/root/auditing-server/src/main/resources/private/clientkeys
